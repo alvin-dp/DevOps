d3d9197c5adbc96f177f6161a8e45ddfd9ba2a88
alvin-dp <alvin.dp@gmail.com>
2019-12-02 16:42:24 +0200

    Добавлена работа с провиженером file + реализован разворот nginx/jenkins без применения темплейтов

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 variable "path_to_key" {
   type = string
 }
+variable "path_to_ngix_repo" {
+  type = string
+}
+variable "path_to_jenkins_repo" {
+  type = string
+}
+variable "path_to_nginx_conf" {
+  type = string
+}
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
@@ -9,6 +18,13 @@ resource "aws_security_group" "allow_ssh" {
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -41,6 +57,9 @@ data "aws_ami" "centos" {
   owners = ["679593333241"]
 }
 
+output "ip" {
+  value = "${aws_instance.web.public_ip}"
+}
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.centos.id}"
@@ -51,9 +70,33 @@ resource "aws_instance" "web" {
     delete_on_termination = true
   }
 #  user_data = "yum update"
+
+  provisioner "file" {
+    source      = var.path_to_nginx_conf
+    destination = "/tmp/nginx_default.conf"
+
+    connection {
+    type     = "ssh"
+    user     = "centos"
+    private_key = file(var.path_to_key)
+    host     = "${self.public_ip}"
+    } 
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo yum -y update",
+      "sudo setenforce 0",
+      "echo '${file(var.path_to_ngix_repo)}' > /tmp/nginx.repo",
+      "sudo cp /tmp/nginx.repo /etc/yum.repos.d/nginx.repo",
+      "echo '${file(var.path_to_jenkins_repo)}' >/tmp/jenkins.repo",
+      "sudo cp /tmp/jenkins.repo /etc/yum.repos.d/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum -y install nginx",      
+      "sudo yum -y install java-1.8.0-openjdk",
+      "sudo yum -y install jenkins",
+      "sudo cp /tmp/nginx_default.conf /etc/nginx/conf.d/default.conf",
+      "sudo service jenkins start",
+      "sudo systemctl start nginx",
      ]
     connection {
     type     = "ssh"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,4 @@
 path_to_key = "~/aws/ssh_main_key.pem"
+path_to_ngix_repo = "~/aws/nginx.repo"
+path_to_jenkins_repo = "~/aws/jenkins.repo"
+path_to_nginx_conf = "~/aws/default.conf"
